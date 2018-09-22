c066834b3adcf79456116bf4b6d2158d5fd25e1e
srimani-programmer <srimani.scholar@gmail.com>
2018-09-22 21:48:44 +0530

    Introduction Concepts

---
+++ Basics/Introduction01.py
@@ -0,0 +1,11 @@
+from matplotlib import pyplot as plt
+import numpy as np
+
+x = np.arange(0,20,0.01)
+y = 1 + np.sin(2 * np.pi * x)
+
+plt.plot(x,y,color='red')
+plt.xlabel('time')
+plt.ylabel('voltage')
+
+plt.show()
